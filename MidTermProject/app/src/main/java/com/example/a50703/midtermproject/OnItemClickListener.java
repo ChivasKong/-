9ace19c5d4da98c91c1f8b734c1f3c972c3b9011
Scourie <507032135@qq.com>
2017-11-26 08:20:54 +0800

    Version2

---
+++ MidTermProject/app/src/main/java/com/example/a50703/midtermproject/OnItemClickListener.java
@@ -0,0 +1,10 @@
+package com.example.a50703.midtermproject;
+
+/**
+ * Created by 50703 on 2017/11/25.
+ */
+
+public interface OnItemClickListener{
+    void onClick(int pos);
+    void onLongClick(int pos);
+}
